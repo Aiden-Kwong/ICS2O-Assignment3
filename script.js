ff793d9f44a09a0ec6d1a00fc16b7eea02b99486
AidenKwong <>
2021-06-02 16:34:54 +0000

    javascript linter fix

--- script.js
+++ script.js
@@ -20,51 +20,51 @@ document.getElementById('btn1963').addEventListener('click', myFunction1963)
 document.getElementById('btn1989').addEventListener('click', myFunction1989)
 
 // When the user clicks on the button, this toggles displaying the content.
-function myFunction1910() {
-  document.getElementById('dropdown-1910').classList.toggle('show');
+function myFunction1910 () {
+  document.getElementById('dropdown-1910').classList.toggle('show')
 }
 
 // This is a repeat of above for the next button.
-function myFunction1928() {
-  document.getElementById('dropdown-1928').classList.toggle('show');
+function myFunction1928 () {
+  document.getElementById('dropdown-1928').classList.toggle('show')
 }
 
 // This is a repeat again, for the next button.
-function myFunction1932() {
-  document.getElementById('dropdown-1932').classList.toggle('show');
+function myFunction1932 () {
+  document.getElementById('dropdown-1932').classList.toggle('show')
 }
 
 // There are 9 of these buttons, the repeats are so that they do not trigger each other.
-function myFunction1936() {
-  document.getElementById('dropdown-1936').classList.toggle('show');
+function myFunction1936 () {
+  document.getElementById('dropdown-1936').classList.toggle('show')
 }
 
 // Toggles displaying the content of this button.
-function myFunction1939() {
-  document.getElementById('dropdown-1939').classList.toggle('show');
+function myFunction1939 () {
+  document.getElementById('dropdown-1939').classList.toggle('show')
 }
 
 // Toggles displaying the content of this button.
-function myFunction1947() {
-  document.getElementById('dropdown-1947').classList.toggle('show');
+function myFunction1947 () {
+  document.getElementById('dropdown-1947').classList.toggle('show')
 }
 
 // Toggles displaying the content of this button.
-function myFunction1955() {
-  document.getElementById('dropdown-1955').classList.toggle('show');
+function myFunction1955 () {
+  document.getElementById('dropdown-1955').classList.toggle('show')
 }
 
 // Toggles displaying the content of this button.
-function myFunction1956() {
-  document.getElementById('dropdown-1956').classList.toggle('show');
+function myFunction1956 () {
+  document.getElementById('dropdown-1956').classList.toggle('show')
 }
 
 // Toggles displaying the content of this button.
-function myFunction1963() {
-  document.getElementById('dropdown-1963').classList.toggle('show');
+function myFunction1963 () {
+  document.getElementById('dropdown-1963').classList.toggle('show')
 }
 
 // Again, toggles displaying the content of this button.
-function myFunction1989() {
-  document.getElementById('dropdown-1989').classList.toggle('show');
+function myFunction1989 () {
+  document.getElementById('dropdown-1989').classList.toggle('show')
 }
